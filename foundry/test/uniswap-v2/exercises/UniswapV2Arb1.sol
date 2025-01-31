bd67f5aef5f6bd0f800cb5456f542ce826e84878
toke82 <adrian.casal.rodriguez@gmail.com>
2025-02-01 00:21:05 +0100

    arbitrage 1

--- foundry/test/uniswap-v2/exercises/UniswapV2Arb1.sol
+++ foundry/test/uniswap-v2/exercises/UniswapV2Arb1.sol
@@ -23,6 +23,42 @@ contract UniswapV2Arb1 {
         uint256 minProfit;
     }
 
+    function _swap(SwapParams memory params) 
+        private 
+        returns (uint256 amountOut) 
+    {
+        // Swap on router0 (tokenIn --> tokenOut)
+        IERC20(params.tokenIn).approve(address(params.router0), params.amountIn);
+
+        address[] memory path = new address[](2);
+        path[0] = params.tokenIn;
+        path[1] = params.tokenOut;
+        
+        uint256[] memory amounts = IUniswapV2Router02(params.router0).swapExactTokensForTokens({
+            amountIn: params.amountIn, 
+            amountOutMin: 0, 
+            path: path, 
+            to: address(this), 
+            deadline: block.timestamp
+        });
+
+        // Swap on router1 (tokenOut --> tokenIn)
+        IERC20(params.tokenOut).approve(address(params.router1), amounts[1]);
+
+        path[0] = params.tokenOut;
+        path[1] = params.tokenIn;
+
+        amounts = IUniswapV2Router02(params.router1).swapExactTokensForTokens({
+            amountIn: amounts[1], 
+            amountOutMin: params.amountIn, 
+            path: path, 
+            to: address(this), 
+            deadline: block.timestamp
+        });
+
+        amountOut = amounts[1];
+    }
+
     // Exercise 1
     // - Execute an arbitrage between router0 and router1
     // - Pull tokenIn from msg.sender
@@ -30,6 +66,12 @@ contract UniswapV2Arb1 {
     function swap(SwapParams calldata params) external {
         // Write your code here
         // Don’t change any other code
+        IERC20(params.tokenIn).transferFrom(
+            msg.sender, address(this), params.amountIn
+        );
+        uint amountOut = _swap(params);
+        require(amountOut - params.amountIn >= params.minProfit, "profit < min");
+        IERC20(params.tokenIn).transfer(msg.sender, amountOut);
     }
 
     // Exercise 2
@@ -43,9 +85,17 @@ contract UniswapV2Arb1 {
      */
     function flashSwap(address pair, bool isToken0, SwapParams calldata params)
         external
-    {
+    {        
         // Write your code here
         // Don’t change any other code
+        bytes memory data = abi.encode(msg.sender, pair, params);
+        
+        IUniswapV2Pair(pair).swap({
+            amount0Out: isToken0 ? params.amountIn : 0,
+            amount1Out: isToken0 ? 0 : params.amountIn,
+            to: address(this),
+            data: data
+        });
     }
 
     function uniswapV2Call(
@@ -56,5 +106,18 @@ contract UniswapV2Arb1 {
     ) external {
         // Write your code here
         // Don’t change any other code
+        (address caller, address pair, SwapParams memory params) = 
+            abi.decode(data, (address, address, SwapParams));
+
+        uint256 amountOut = _swap(params);
+
+        uint256 fee = ((params.amountIn * 3) / 997) + 1;
+        uint256 amountToRepay = params.amountIn + fee;
+
+        uint256 profit = amountOut - amountToRepay;
+        require(profit >= params.minProfit, "profit < min");
+
+        IERC20(params.tokenIn).transfer(address(pair), amountToRepay);
+        IERC20(params.tokenIn).transfer(caller, profit);
     }
 }
